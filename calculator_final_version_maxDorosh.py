ebc282c4db4dc97eb14160d994c6c994819a6f56
maxorex <maxorextop@gmail.com>
2024-10-29 18:30:11 +0000

    Update1 calculator_final_version_maxDorosh.py
    adicionei emojis, paragrafos, adicionei uma historia completo e o aspeto do coluna

--- calculator_final_version_maxDorosh.py
+++ calculator_final_version_maxDorosh.py
@@ -19,59 +19,60 @@ def multiplicaçao(x, y):
 history = []
 
 def calculadora():
-    num1 = (input('Insert first number: '))
+    num1 = (input('\nInsert first number: '))
     try:
         val = float(num1)
     except ValueError:
-     print('invalid number')
+     print('⚠️invalid number⚠️')
      calculadora()
 
-    a = input('Indicate your sinal: +, -, /, *, : ')
+    a = input('\nIndicate your sinal: +, -, /, *, : ')
 
-    num2 = (input('Insert second number: '))
+    num2 = (input('\nInsert second number: '))
     try:
         val = float(num2)
     except ValueError:
-        print('invalid number')
+        print('⚠️invalid number⚠️')
         calculadora()
 
     if a == '+':
         result = soma(float(num1), float(num2))
-        print('Result of amount: ' + str(result))
-        history.insert(0,result)
+        print('\nResult of amount: ' + str(result))
+        history.insert(0,f'{num1} {a} {num2} = {result}')
 
     if a == '-':
         result1 = subtraçao(float(num1), float(num2))
-        print('Result of subtraction: ' + str(result1))
-        history.insert(0, result1)
+        print('\nResult of subtraction: ' + str(result1))
+        history.insert(0, f'{num1} {a} {num2} = {result1}')
 
     if a == '/':
         result2 = divisao(float(num1), float(num2))
-        print('Result of division: ' + str(result2))
-        history.insert(0, result2)
+        print('\nResult of division: ' + str(result2))
+        history.insert(0, f'{num1} {a} {num2} = {result2}')
 
     if a == '*':
         result3 = multiplicaçao(float(num1), float(num2))
-        print('Result of multiplication: ' + str(result3))
-        history.insert(0, result3)
+        print('\nResult of multiplication: ' + str(result3))
+        history.insert(0, f'{num1} {a} {num2} = {result3}')
 
     if a != '+' and a != '-' and a != '*' and a != '/':
-        print(' !ALER!try again The problem in +, -, *, or / ... ')
+        print('\n⚠️!ALER!⚠️try again\nThe problem in +, -, *, or / ...\n')
         calculadora()
     else: pass
 
     if len(history) >= 6:
         history.pop(5)
 
-    e = input("Show history? yes/no: ")
+    e = input("\nShow history? \nyes/no: ")
     if e == 'yes' or e == 'Yes' or e == 'YES':
-        print(record)
+        for record in history:
+            print(record)
     else:
         pass
 
 
 
-    b = input("Stop? yes/no: ")
+    b = input("\nStop? \nyes/no: ")
     if b == 'yes' or b == 'Yes' or b == 'YES':
         shutdown()
     else:
